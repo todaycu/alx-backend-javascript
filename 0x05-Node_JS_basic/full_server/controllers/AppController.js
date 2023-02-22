0a1d1a3f1a99febc2c573789e30314e7d967e95d
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2023-02-22 21:59:44 +0300

    Update AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,12 @@
-module.exports = class AppController {
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+class AppController {
   static getHomepage(request, response) {
     response.status(200).send('Hello Holberton School!');
   }
-};
+}
+
+export default AppController;
+module.exports = AppController;
